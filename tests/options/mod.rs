10ec04c32cd1e133c66de04283f27518ab870808
Haixing Hu <starfish.hu@gmail.com>
2026-04-26 19:26:01 +0800

    test(retry_delay): move Display/FromStr tests to retry_delay_duration_format_tests
    - Keep serde/validation in retry_delay_tests
    
    - Add tests/options/retry_delay_duration_format_tests

--- tests/options/mod.rs
+++ tests/options/mod.rs
@@ -11,6 +11,7 @@ mod attempt_timeout_option_tests;
 mod attempt_timeout_policy_tests;
 #[cfg(feature = "config")]
 mod retry_config_values_tests;
+mod retry_delay_duration_format_tests;
 mod retry_delay_tests;
 mod retry_jitter_tests;
 #[cfg(feature = "config")]
