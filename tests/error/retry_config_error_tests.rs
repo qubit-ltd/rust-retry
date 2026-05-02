5aebdce86cd52fe9b4acd1991e773a5852e2a1ed
Haixing Hu <starfish.hu@gmail.com>
2026-05-02 16:16:52 +0800

    docs: normalize retry config test license header

--- tests/error/retry_config_error_tests.rs
+++ tests/error/retry_config_error_tests.rs
@@ -1,12 +1,12 @@
 /*******************************************************************************
  *
- *    Copyright (c) 2025 - 2026.
- *    Haixing Hu, Qubit Co. Ltd.
+ *    Copyright (c) 2025 - 2026 Haixing Hu.
  *
- *    All rights reserved.
+ *    SPDX-License-Identifier: Apache-2.0
  *
-******************************************************************************/
-
+ *    Licensed under the Apache License, Version 2.0.
+ *
+ ******************************************************************************/
 use qubit_common::DataType;
 
 /// Verifies configuration error display output for empty and non-empty paths.
